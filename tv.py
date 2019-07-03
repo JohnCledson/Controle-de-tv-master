19e94bc8f4d2e6dd33a62b531c4f56b3e30f743b
John Cledson <cledsonjohn123@gmail.com>
2019-07-03 01:56:22 -0300

    Funcao channel

--- tv.py
+++ tv.py
@@ -1,13 +1,23 @@
 class Tv:
     def __init__(self):
         self.status = 'Desligada'
+        self.channel = 5
     def power(self):
         if self.status:
             self.status = 'Ligando'
         else:
             self.status = 'Desligando'
+    def channelMore(self):
+        self.channel += 1
+    def channelLess(self):
+        self.channel -= 1
 
 tv = Tv()
 print(tv.status)
 tv.power()
 print(tv.status)
+print(tv.channel)
+tv.channelLess()
+print(tv.channel)
+tv.channelMore()
+print(tv.channel)
